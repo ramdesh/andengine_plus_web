9674641dc8a67ab1f576f91381201de8fa0945df
Ramindu Deshapriya <rasade88@gmail.com>
2013-05-19 00:08:20 +0530

    Resolved draggable remove

--- js/andengine_plus_web.js
+++ js/andengine_plus_web.js
@@ -7,12 +7,17 @@
 
 function initializeDroppable() {
 	$( ".screen" ).droppable({
+		  tolerance: "fit",
 	      drop: function( event, ui ) {
 	        $( this )
 	          //.addClass( "ui-state-highlight" )
 	          .append("<input type=\"text\" placeholder=\"Enter sprite name here\" class=\"text\" id=\"contact-name\" />");
 	        ui.draggable.addClass("on-screen");
-	      }
+	      },
+		  out: function( event,ui ) {
+			$(this).html("");
+			ui.draggable.removeClass("on-screen"); 
+		  }
 	    });
 }
 function save() {
