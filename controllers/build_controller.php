4901007fb2e3b95d5369e7aa51f34e36a50074c2
ramdesh <rasade88@gmail.com>
2013-06-22 15:43:56 +0530

    Added resource addition to XML creation

--- controllers/build_controller.php
+++ controllers/build_controller.php
@@ -16,9 +16,23 @@ class BuildController {
         $this->build_xml();
     }
     private function build_xml() {
+        $query = "SELECT *
+                  FROM project_log
+                  WHERE project_id=1";
+        $this->_build_db->setQuery($query);
+        $contents = $this->_build_db->loadAssocList();
         $xmldoc = new DOMDocument();
         $xmldoc->formatOutput = true;
         $gameElement = $xmldoc->createElement("game");
+        $resources = $gameElement->appendChild("resources");
+
+        for ( $i = 0; $i < count($contents); $i++ ) {
+            if ( $contents[$i]['activity'] == "sprite_position" ) {
+                $params = json_decode($contents[$i]['activity_params']);
+                $single_resource = $resources->appendChild("resource");
+                $single_resource->appendChild($xmldoc->createTextNode($params['resource']));
+            }
+        }
 
         $xmldoc->save("../xmloutput/test_project.xml");
     }
